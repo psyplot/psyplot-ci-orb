f187b75be15e775efd8f53ace9205bdfa24498ad
Philipp S. Sommer <philipp.sommer@hzg.de>
2020-11-02 20:01:27 +0100

    add python package for deployment

---
+++ src/scripts/deploy_pkg.sh
@@ -0,0 +1,10 @@
+deploy_pkg() {
+    # deploy to package to the anaconda channel
+}
+
+# Will not run if sourced for bats-core tests.
+# View src/tests for more information.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    deploy_pkg
+fi
