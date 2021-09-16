c29161432f4660bf31a822e30171046a30195f94
Philipp Sommer <philipp.sommer@hereon.de>
2021-09-16 18:06:07 +0200

    add command to setup a conda environment
    by default from ci/environment.yml

---
+++ src/scripts/setup-conda-env.sh
@@ -0,0 +1,15 @@
+setup-conda-env() {
+
+    CONDADIR=$(eval echo "$CONDADIR")
+    which conda || eval "$("${CONDADIR}"/bin/conda shell.bash hook)"
+
+    conda env create -n "${CONDAENV_NAME}" -f "${CONDAENV_FILE}"
+
+}
+
+# Will not run if sourced for bats-core tests.
+# View src/tests for more information.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    setup-conda-env
+fi
