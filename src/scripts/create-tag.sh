951297905af849b26f7e9b6d46a3b3597b13a3bc
Philipp Sommer <philipp.sommer@hereon.de>
2021-09-23 17:22:56 +0200

    add command to create tags

---
+++ src/scripts/create-tag.sh
@@ -0,0 +1,81 @@
+
+GetIncrement() {
+    COMMIT_SUBJECT=$(git log -1 --pretty=%s)
+    RELEASE_MESSAGE="$(git log -1 --pretty=%b)"
+    SEMVER_INCREMENT=$(echo "${COMMIT_SUBJECT}" | sed -En 's/.*\[semver:(major|minor|patch|skip)\].*/\1/p')
+    echo "Commit subject: ${COMMIT_SUBJECT}"
+    echo "export SEMVER_INCREMENT=\"$SEMVER_INCREMENT\""  >> "$BASH_ENV"
+}
+
+CheckIncrement() {
+    if [ -z "${SEMVER_INCREMENT}" ];then
+        echo "Commit subject did not indicate which SemVer increment to make."
+        echo "To publish orb, you can ammend the commit or push another commit with [semver:FOO] in the subject where FOO is major, minor, patch."
+        echo "Note: To indicate intention to skip promotion, include [semver:skip] in the commit subject instead."
+        if [ "$SHOULD_FAIL" == "1" ];then
+            exit 1
+        else
+        echo "export PR_MESSAGE=\"BotComment: Package release was skipped due to [semver:patch|minor|major] not being included in commit message.\""  >> "$BASH_ENV"
+        fi
+    elif [ "$SEMVER_INCREMENT" == "skip" ];then
+        echo "SEMVER in commit indicated to skip orb release"
+        echo "export PR_MESSAGE=\"BotComment: Package release was skipped due to [semver:skip] in commit message.\""  >> "$BASH_ENV"
+    fi
+}
+
+JoinVersion() {
+  local IFS="."
+  echo "$*"
+}
+
+DoIncrement() {
+
+    LATEST_TAG="$(git describe --abbrev=0 --tags)"
+    if [[ "${TAG_PREFIX}" != "" ]]; then
+      LATEST_VERSION="${LATEST_TAG:${#TAG_PREFIX}}"
+    else
+      LATEST_VERSION="${LATEST_TAG}"
+    fi
+    IFS="." read -r -a VERSION_PARTS <<< "${LATEST_VERSION}"
+
+    if [ "$SEMVER_INCREMENT" == "major" ]; then
+        VERSION_PARTS[0]=$((VERSION_PARTS[0]+1))
+    elif [ "$SEMVER_INCREMENT" == "minor" ]; then
+        VERSION_PARTS[1]=$((VERSION_PARTS[1]+1))
+    elif [ "$SEMVER_INCREMENT" == "patch" ]; then
+        VERSION_PARTS[2]=$((VERSION_PARTS[2]+1))
+    fi
+    NEW_VERSION=$(JoinVersion "${VERSION_PARTS[@]}")
+    NEW_TAG="${TAG_PREFIX}${NEW_VERSION}"
+
+    RELEASE_TITLE="$(echo "${COMMIT_SUBJECT}" | sed 's/\s*\[semver:.*\]\s*//')"
+    echo "${CLEAN_MESSAGE}"
+
+
+    git tag -a "${NEW_TAG}" \
+      -m "${RELEASE_TITLE}" \
+      -m "${RELEASE_MESSAGE}"
+
+    echo "export NEW_VERSION=\"${NEW_VERSION}\"" >> "$BASH_ENV"
+    echo "export NEW_TAG=\"${NEW_TAG}\"" >> "$BASH_ENV"
+    echo "export RELEASE_TITLE=\"${RELEASE_TITLE}\"" >> "$BASH_ENV"
+
+    # insert newline characters into variable
+    RELEASE_MESSAGE=$(echo "${RELEASE_MESSAGE}" | sed -E ':a;N;$!ba;s/\r{0,1}\n/\\n/g')
+    echo "export RELEASE_MESSAGE=\"${RELEASE_MESSAGE}\"" >> "$BASH_ENV"
+    echo "export PR_MESSAGE=\"BotComment: *Production* version of package available for use - \\\`${NEW_TAG}\\\`\"" >> "$BASH_ENV"
+}
+
+create-tag() {
+    # create a tag based on git
+    GetIncrement
+    CheckIncrement
+    [ "$SEMVER_INCREMENT" == "skip" ] || DoIncrement
+}
+
+# Will not run if sourced for bats-core tests.
+# View src/tests for more information.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    create-tag
+fi
---
+++ src/tests/12-create-tag.bats
@@ -0,0 +1,65 @@
+# Runs prior to every test
+setup() {
+
+    export WORKDIR="${BATS_TMPDIR}/test-repo/"
+    mkdir -p "${WORKDIR}"
+
+    git init "${WORKDIR}"
+
+    touch "${WORKDIR}"/dummy_file.md
+    git -C "${WORKDIR}" add dummy_file.md
+
+    git -C "${WORKDIR}" commit -m "Add dummy file"
+    git -C "${WORKDIR}" tag -a v0.0.1 -m "Test commit"
+
+
+    source ./src/scripts/create-tag.sh
+}
+
+@test 'Test creating a patch' {
+
+    cd "${WORKDIR}"
+
+    git commit --allow-empty \
+      -m "[semver:patch] Test patch
+
+This is the description"
+
+    BASH_ENV="bash.sh" TAG_PREFIX="v" create-tag && \
+    cat bash.sh && \
+    source bash.sh && \
+    [ "${NEW_VERSION}" == "0.0.2" ] && \
+    [ "${NEW_TAG}" == "v0.0.2" ] && \
+    [ "${RELEASE_TITLE}" == "Test patch" ] && \
+    [ "${RELEASE_MESSAGE}" == "This is the description" ]
+
+    git commit --allow-empty \
+      -m "[semver:minor] Test minor
+
+This is the description"
+
+    BASH_ENV="bash.sh" TAG_PREFIX="v" create-tag && \
+    cat bash.sh && \
+    source bash.sh && \
+    [ "${NEW_VERSION}" == "0.1.2" ] && \
+    [ "${NEW_TAG}" == "v0.1.2" ] && \
+    [ "${RELEASE_TITLE}" == "Test minor" ] && \
+    [ "${RELEASE_MESSAGE}" == "This is the description" ]
+
+    git commit --allow-empty \
+      -m "[semver:major] Test major
+
+This is the description"
+
+    BASH_ENV="bash.sh" TAG_PREFIX="v" create-tag && \
+    cat bash.sh && \
+    source bash.sh && \
+    [ "${NEW_VERSION}" == "1.1.2" ] && \
+    [ "${NEW_TAG}" == "v1.1.2" ] && \
+    [ "${RELEASE_TITLE}" == "Test major" ] && \
+    [ "${RELEASE_MESSAGE}" == "This is the description" ]
+}
+
+teardown() {
+    rm -rf "${WORKDIR}"
+}
