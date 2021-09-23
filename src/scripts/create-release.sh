254cfd9cfafa02f1ad72d471fd3fa35b47e9de75
Philipp Sommer <philipp.sommer@hereon.de>
2021-09-23 17:31:03 +0200

    add command to create releases

---
+++ src/scripts/create-release.sh
@@ -0,0 +1,25 @@
+create-release() {
+    [ "${TOKEN}" ] && GITHUB_TOKEN="${TOKEN}"
+    cat > release_body.json << EOF
+{
+    "tag_name": "${NEW_TAG}",
+    "name": "${NEW_TAG}: ${RELEASE_TITLE}",
+    "body": "${RELEASE_MESSAGE}",
+    "draft": ${DRAFT}
+}
+EOF
+    curl \
+        -X POST \
+        -H "Authorization: Token ${GITHUB_TOKEN}" \
+        -H "Content-Type: application/json" \
+        -H "Accept: application/vnd.github.v3+json" \
+        https://api.github.com/repos/${CIRCLE_PROJECT_USERNAME}/${CIRCLE_PROJECT_REPONAME}/releases \
+        -d @release_body.json
+}
+
+# Will not run if sourced for bats-core tests.
+# View src/tests for more information.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    create-tag
+fi
