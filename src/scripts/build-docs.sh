6e3985e9e30aea387fe7c8ed9b5222a8897cd379
Philipp Sommer <philipp.sommer@hereon.de>
2021-10-02 15:53:10 +0200

    minor

--- src/scripts/build-docs.sh
+++ src/scripts/build-docs.sh
@@ -2,12 +2,12 @@ build-docs() {
     CONDADIR=$(eval echo "$CONDADIR")
     which conda || eval "$("${CONDADIR}"/bin/conda shell.bash hook)"
 
-    conda activate ${CONDAENV_NAME}
+    conda activate "${CONDAENV_NAME}"
 
     WORKDIR="$(pwd)"
     cd "${SRC_DIR}" || exit 1
 
-    # shellcheck disable=SC2086
+    # shellcheck disable=SC2086,SC2153
     for BUILDER in ${BUILDERS}; do
         sphinx-build -b "${BUILDER}" -d "${WORKDIR}/${BUILD_DIR}/doctrees" . "${WORKDIR}/${BUILD_DIR}/${BUILDER}"
     done
