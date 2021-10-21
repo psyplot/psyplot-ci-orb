61db24c55ffff80dc65d2d8e0335c67901786e2e
Philipp Sommer <philipp.sommer@hereon.de>
2021-10-21 19:39:15 +0200

    minor

--- src/scripts/conda-info.sh
+++ src/scripts/conda-info.sh
@@ -2,12 +2,12 @@ conda-info() {
     CONDADIR=$(eval echo "$CONDADIR")
     which conda || eval "$("${CONDADIR}"/bin/conda shell.bash hook)"
 
-    conda info
+    conda info -a
 }
 
 # Will not run if sourced for bats-core tests.
 # View src/tests for more information.
 ORB_TEST_ENV="bats-core"
 if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
-    conda info -a
+    conda-info
 fi
