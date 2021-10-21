a1141a7ebf44364506deda507644f35f43e93fd5
Philipp Sommer <philipp.sommer@hereon.de>
2021-10-21 17:52:14 +0200

    add conda info step to configure-conda

---
+++ src/scripts/conda-info.sh
@@ -0,0 +1,13 @@
+conda-info() {
+    CONDADIR=$(eval echo "$CONDADIR")
+    which conda || eval "$("${CONDADIR}"/bin/conda shell.bash hook)"
+
+    conda info
+}
+
+# Will not run if sourced for bats-core tests.
+# View src/tests for more information.
+ORB_TEST_ENV="bats-core"
+if [ "${0#*$ORB_TEST_ENV}" == "$0" ]; then
+    conda info -a
+fi
